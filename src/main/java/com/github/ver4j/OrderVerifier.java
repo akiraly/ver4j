a8ddfb08f18d08c6255aaa89ecfb4434b42672e0
akiraly <kiralyattila.hu@gmail.com>
2011-11-01 18:58:01 +0100

    Adding more order verification methods.

--- src/main/java/com/github/ver4j/OrderVerifier.java
+++ src/main/java/com/github/ver4j/OrderVerifier.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import javax.annotation.Nonnull;
 
+import org.apache.commons.lang3.Range;
+
 public class OrderVerifier<C extends Comparable<C>> extends
 		AObjectVerifierAwareVerifier {
 	private static final String FAILED_IS_BEFORE_CAUSE = "the orderable object is not before the reference";
@@ -15,6 +17,16 @@ public class OrderVerifier<C extends Comparable<C>> extends
 
 	private static final String FAILED_NOT_AFTER_CAUSE = "the orderable object is after the reference";
 
+	private static final String FAILED_EQUALS_CAUSE = "the orderable object is not equal to the reference";
+
+	private static final String FAILED_NOT_EQUAL_CAUSE = "the orderable object is equal to the reference";
+
+	private static final String FAILED_INCLUSIVE_BETWEEN_CAUSE = "the orderable object is not inclusive between the reference range";
+
+	private static final String FAILED_EXCLUSIVE_BETWEEN_CAUSE = "the orderable object is not exclusive between the reference range";
+
+	private static final String FAILED_IN_RANGE_CAUSE = "the orderable object is not contained by the reference range";
+
 	private final ExceptionFactory<?> isBeforeExceptionFactory;
 
 	private final ExceptionFactory<?> isAfterExceptionFactory;
@@ -22,6 +34,16 @@ public class OrderVerifier<C extends Comparable<C>> extends
 	private final ExceptionFactory<?> notBeforeExceptionFactory;
 
 	private final ExceptionFactory<?> notAfterExceptionFactory;
+
+	private final ExceptionFactory<?> equalsExceptionFactory;
+
+	private final ExceptionFactory<?> notEqualExceptionFactory;
+
+	private final ExceptionFactory<?> inclusiveBetweenExceptionFactory;
+
+	private final ExceptionFactory<?> exclusiveBetweenExceptionFactory;
+
+	private final ExceptionFactory<?> inRangeExceptionFactory;
 
 	public OrderVerifier(@Nonnull ObjectVerifier objectVerifier,
 			@Nonnull ExceptionTypeInfo<?> orderExceptionTypeInfo) {
@@ -34,6 +56,16 @@ public class OrderVerifier<C extends Comparable<C>> extends
 				FAILED_NOT_BEFORE_CAUSE);
 		notAfterExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
 				FAILED_NOT_AFTER_CAUSE);
+		equalsExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
+				FAILED_EQUALS_CAUSE);
+		notEqualExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
+				FAILED_NOT_EQUAL_CAUSE);
+		inclusiveBetweenExceptionFactory = exceptionFactoryOf(
+				orderExceptionTypeInfo, FAILED_INCLUSIVE_BETWEEN_CAUSE);
+		exclusiveBetweenExceptionFactory = exceptionFactoryOf(
+				orderExceptionTypeInfo, FAILED_EXCLUSIVE_BETWEEN_CAUSE);
+		inRangeExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
+				FAILED_IN_RANGE_CAUSE);
 	}
 
 	@Nonnull
@@ -193,6 +225,84 @@ public class OrderVerifier<C extends Comparable<C>> extends
 	}
 
 	@Nonnull
+	public final <T extends C> T equals(@Nonnull T comparable,
+			@Nonnull T reference) {
+		object().notNull(comparable);
+		object().notNull(reference);
+		if (isDisabled() || comparable.compareTo(reference) == 0)
+			return comparable;
+		throw equalsExceptionFactory.newException(
+				newComparableContext(comparable, reference), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T equals(@Nonnull T comparable,
+			@Nonnull T reference, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(reference, errorMessage, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) == 0)
+			return comparable;
+		throw equalsExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, reference),
+				null);
+	}
+
+	@Nonnull
+	public final <T extends C> T equalsCm(@Nonnull T comparable,
+			@Nonnull T reference, String errorMessageTemplate, Locale locale,
+			Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(reference, errorMessageTemplate, locale,
+				errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) == 0)
+			return comparable;
+		throw equalsExceptionFactory.newExceptionCm(errorMessageTemplate,
+				locale, errorMessageArgs,
+				newComparableContext(comparable, reference), null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notEqual(@Nonnull T comparable,
+			@Nonnull T reference) {
+		object().notNull(comparable);
+		object().notNull(reference);
+		if (isDisabled() || comparable.compareTo(reference) != 0)
+			return comparable;
+		throw notEqualExceptionFactory.newException(
+				newComparableContext(comparable, reference), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notEqual(@Nonnull T comparable,
+			@Nonnull T reference, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(reference, errorMessage, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) != 0)
+			return comparable;
+		throw notEqualExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, reference),
+				null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notEqualCm(@Nonnull T comparable,
+			@Nonnull T reference, String errorMessageTemplate, Locale locale,
+			Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(reference, errorMessageTemplate, locale,
+				errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) != 0)
+			return comparable;
+		throw notEqualExceptionFactory.newExceptionCm(errorMessageTemplate,
+				locale, errorMessageArgs,
+				newComparableContext(comparable, reference), null);
+	}
+
+	@Nonnull
 	private final <T extends C> Map<Object, Object> newComparableContext(
 			@Nonnull T comparable, @Nonnull T reference) {
 		Map<Object, Object> context = object().newContextMap();
@@ -201,8 +311,149 @@ public class OrderVerifier<C extends Comparable<C>> extends
 		return context;
 	}
 
-	/*
-	 * inclusiveBetween, exclusiveBetween, in + range, greaterThan, lessThan,
-	 * notGreaterThan, notLessThan, orderEquals, notOrderEquals
-	 */
+	@Nonnull
+	public final <T extends C> T inclusiveBetween(@Nonnull T comparable,
+			@Nonnull T min, @Nonnull T max) {
+		object().notNull(comparable);
+		object().notNull(min);
+		object().notNull(max);
+		if (isDisabled() || comparable.compareTo(min) >= 0
+				&& comparable.compareTo(max) <= 0)
+			return comparable;
+		throw inclusiveBetweenExceptionFactory.newException(
+				newComparableContext(comparable, min, max), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T inclusiveBetween(@Nonnull T comparable,
+			@Nonnull T min, @Nonnull T max, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(min, errorMessage, errorMessageArgs);
+		object().notNull(max, errorMessage, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(min) >= 0
+				&& comparable.compareTo(max) <= 0)
+			return comparable;
+		throw inclusiveBetweenExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, min, max),
+				(Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T inclusiveBetweenCm(@Nonnull T comparable,
+			@Nonnull T min, @Nonnull T max, String errorMessageTemplate,
+			Locale locale, Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(min, errorMessageTemplate, locale, errorMessageArgs);
+		object().notNullCm(max, errorMessageTemplate, locale, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(min) >= 0
+				&& comparable.compareTo(max) <= 0)
+			return comparable;
+		throw inclusiveBetweenExceptionFactory.newExceptionCm(
+				errorMessageTemplate, locale, errorMessageArgs,
+				newComparableContext(comparable, min, max), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T exclusiveBetween(@Nonnull T comparable,
+			@Nonnull T min, @Nonnull T max) {
+		object().notNull(comparable);
+		object().notNull(min);
+		object().notNull(max);
+		if (isDisabled() || comparable.compareTo(min) > 0
+				&& comparable.compareTo(max) < 0)
+			return comparable;
+		throw exclusiveBetweenExceptionFactory.newException(
+				newComparableContext(comparable, min, max), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T exclusiveBetween(@Nonnull T comparable,
+			@Nonnull T min, @Nonnull T max, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(min, errorMessage, errorMessageArgs);
+		object().notNull(max, errorMessage, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(min) > 0
+				&& comparable.compareTo(max) < 0)
+			return comparable;
+		throw exclusiveBetweenExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, min, max),
+				(Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T exclusiveBetweenCm(@Nonnull T comparable,
+			@Nonnull T min, @Nonnull T max, String errorMessageTemplate,
+			Locale locale, Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(min, errorMessageTemplate, locale, errorMessageArgs);
+		object().notNullCm(max, errorMessageTemplate, locale, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(min) > 0
+				&& comparable.compareTo(max) < 0)
+			return comparable;
+		throw exclusiveBetweenExceptionFactory.newExceptionCm(
+				errorMessageTemplate, locale, errorMessageArgs,
+				newComparableContext(comparable, min, max), (Throwable) null);
+	}
+
+	@Nonnull
+	private final <T extends C> Map<Object, Object> newComparableContext(
+			@Nonnull T comparable, @Nonnull T min, @Nonnull T max) {
+		Map<Object, Object> context = object().newContextMap();
+		context.put("tested object", comparable);
+		context.put("reference minimum", min);
+		context.put("reference maximum", max);
+		return context;
+	}
+
+	@Nonnull
+	public final <T extends C> T inRange(@Nonnull T comparable,
+			@Nonnull Range<T> range) {
+		object().notNull(comparable);
+		object().notNull(range);
+		if (isDisabled() || range.contains(comparable))
+			return comparable;
+		throw inRangeExceptionFactory.newException(
+				newComparableContext(comparable, range), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T inRange(@Nonnull T comparable,
+			@Nonnull Range<T> range, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(range, errorMessage, errorMessageArgs);
+		if (isDisabled() || range.contains(comparable))
+			return comparable;
+		throw inRangeExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, range),
+				(Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T inRangeCm(@Nonnull T comparable,
+			@Nonnull Range<T> range, String errorMessageTemplate,
+			Locale locale, Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(range, errorMessageTemplate, locale,
+				errorMessageArgs);
+		if (isDisabled() || range.contains(comparable))
+			return comparable;
+		throw inRangeExceptionFactory.newExceptionCm(errorMessageTemplate,
+				locale, errorMessageArgs,
+				newComparableContext(comparable, range), (Throwable) null);
+	}
+
+	@Nonnull
+	private final <T extends C> Map<Object, Object> newComparableContext(
+			@Nonnull T comparable, @Nonnull Range<T> range) {
+		Map<Object, Object> context = object().newContextMap();
+		context.put("tested object", comparable);
+		context.put("reference range", range);
+		return context;
+	}
 }
--- src/test/java/com/github/ver4j/OrderVerifierTest.java
+++ src/test/java/com/github/ver4j/OrderVerifierTest.java
@@ -2,6 +2,7 @@ package com.github.ver4j;
 
 import junit.framework.Assert;
 
+import org.apache.commons.lang3.Range;
 import org.junit.Test;
 
 import com.github.ver4j.arg.exception.ArgumentOrderVerificationException;
@@ -51,7 +52,7 @@ public class OrderVerifierTest {
 
 	@Test(expected = ArgumentOrderVerificationException.class)
 	public void testIsBeforeWithFalse2() {
-		verifier.isBefore(1, 0);
+		verifier.isBefore(1, 0, "");
 	}
 
 	@Test(expected = ArgumentOrderVerificationException.class)
@@ -94,7 +95,7 @@ public class OrderVerifierTest {
 
 	@Test(expected = ArgumentOrderVerificationException.class)
 	public void testIsAfterWithFalse2() {
-		verifier.isAfter(1, 2);
+		verifier.isAfter(1, 2, "");
 	}
 
 	@Test(expected = ArgumentOrderVerificationException.class)
@@ -137,7 +138,7 @@ public class OrderVerifierTest {
 
 	@Test(expected = ArgumentOrderVerificationException.class)
 	public void testNotBeforeWithFalse2() {
-		verifier.notBefore(1, 2);
+		verifier.notBefore(1, 2, "");
 	}
 
 	@Test(expected = ArgumentOrderVerificationException.class)
@@ -180,7 +181,7 @@ public class OrderVerifierTest {
 
 	@Test(expected = ArgumentOrderVerificationException.class)
 	public void testNotAfterWithFalse2() {
-		verifier.notAfter(1, 0);
+		verifier.notAfter(1, 0, "");
 	}
 
 	@Test(expected = ArgumentOrderVerificationException.class)
@@ -191,6 +192,225 @@ public class OrderVerifierTest {
 	@Test(expected = ArgumentOrderVerificationException.class)
 	public void testNotAfterCmWithFalse() {
 		verifier.notAfterCm(1, 0, "", null);
+	}
+
+	// equals*
+
+	@Test
+	public void testEqualsWithTrue() {
+		Integer i = 1;
+		Integer r = 1;
+		Assert.assertSame(i, verifier.equals(i, r));
+		Assert.assertSame(i, verifier.equals(i, r));
+		Assert.assertSame(i, verifier.equals(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.equalsCm(i, r, "", null));
+	}
+
+	@Test
+	public void testEqualsWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 1;
+		Integer r = 0;
+		Assert.assertSame(i, verifier.equals(i, r));
+		Assert.assertSame(i, verifier.equals(i, r));
+		Assert.assertSame(i, verifier.equals(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.equalsCm(i, r, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testEqualsWithFalse1() {
+		verifier.equals(1, 0);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testEqualsWithFalse2() {
+		verifier.equals(1, 0, "");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testEqualsWithFalse3() {
+		verifier.equals(1, 0, "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testEqualsCmWithFalse() {
+		verifier.equalsCm(1, 0, "", null);
+	}
+
+	// notEqual*
+
+	@Test
+	public void testNotEqualWithTrue() {
+		Integer i = 1;
+		Integer r = 0;
+		Assert.assertSame(i, verifier.notEqual(i, r));
+		Assert.assertSame(i, verifier.notEqual(i, r));
+		Assert.assertSame(i, verifier.notEqual(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.notEqualCm(i, r, "", null));
+	}
+
+	@Test
+	public void testNotEqualWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 1;
+		Integer r = 1;
+		Assert.assertSame(i, verifier.notEqual(i, r));
+		Assert.assertSame(i, verifier.notEqual(i, r));
+		Assert.assertSame(i, verifier.notEqual(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.notEqualCm(i, r, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotEqualWithFalse1() {
+		verifier.notEqual(1, 1);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotEqualWithFalse2() {
+		verifier.notEqual(1, 1, "");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotEqualWithFalse3() {
+		verifier.notEqual(1, 1, "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotEqualCmWithFalse() {
+		verifier.notEqualCm(1, 1, "", null);
+	}
+
+	// inclusiveBetween*
+
+	@Test
+	public void testInclusiveBetweenWithTrue() {
+		Integer i = 1;
+		Integer min = 1;
+		Integer max = 2;
+		Assert.assertSame(i, verifier.inclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.inclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.inclusiveBetween(i, min, max, "%s", "1"));
+		Assert.assertSame(i, verifier.inclusiveBetweenCm(i, min, max, "", null));
+	}
+
+	@Test
+	public void testInclusiveBetweenWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 0;
+		Integer min = 1;
+		Integer max = 2;
+		Assert.assertSame(i, verifier.inclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.inclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.inclusiveBetween(i, min, max, "%s", "1"));
+		Assert.assertSame(i, verifier.inclusiveBetweenCm(i, min, max, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInclusiveBetweenWithFalse1() {
+		verifier.inclusiveBetween(0, 1, 2);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInclusiveBetweenWithFalse2() {
+		verifier.inclusiveBetween(0, 1, 2, "");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInclusiveBetweenWithFalse3() {
+		verifier.inclusiveBetween(0, 1, 2, "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInclusiveBetweenCmWithFalse() {
+		verifier.inclusiveBetweenCm(0, 1, 2, "", null);
+	}
+
+	// exclusiveBetween*
+
+	@Test
+	public void testExclusiveBetweenWithTrue() {
+		Integer i = 1;
+		Integer min = 0;
+		Integer max = 2;
+		Assert.assertSame(i, verifier.exclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.exclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.exclusiveBetween(i, min, max, "%s", "1"));
+		Assert.assertSame(i, verifier.exclusiveBetweenCm(i, min, max, "", null));
+	}
+
+	@Test
+	public void testExclusiveBetweenWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 0;
+		Integer min = 0;
+		Integer max = 2;
+		Assert.assertSame(i, verifier.exclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.exclusiveBetween(i, min, max));
+		Assert.assertSame(i, verifier.exclusiveBetween(i, min, max, "%s", "1"));
+		Assert.assertSame(i, verifier.exclusiveBetweenCm(i, min, max, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testExclusiveBetweenWithFalse1() {
+		verifier.exclusiveBetween(0, 0, 2);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testExclusiveBetweenWithFalse2() {
+		verifier.exclusiveBetween(0, 0, 2, "");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testExclusiveBetweenWithFalse3() {
+		verifier.exclusiveBetween(0, 0, 2, "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testExclusiveBetweenCmWithFalse() {
+		verifier.exclusiveBetweenCm(0, 0, 2, "", null);
+	}
+
+	// inRange*
+
+	@Test
+	public void testInRangeWithTrue() {
+		Integer i = 1;
+		Range<Integer> range = Range.between(1, 2);
+		Assert.assertSame(i, verifier.inRange(i, range));
+		Assert.assertSame(i, verifier.inRange(i, range));
+		Assert.assertSame(i, verifier.inRange(i, range, "%s", "1"));
+		Assert.assertSame(i, verifier.inRangeCm(i, range, "", null));
+	}
+
+	@Test
+	public void testInRangeWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 0;
+		Range<Integer> range = Range.between(1, 2);
+		Assert.assertSame(i, verifier.inRange(i, range));
+		Assert.assertSame(i, verifier.inRange(i, range));
+		Assert.assertSame(i, verifier.inRange(i, range, "%s", "1"));
+		Assert.assertSame(i, verifier.inRangeCm(i, range, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInRangeWithFalse1() {
+		verifier.inRange(0, Range.between(1, 2));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInRangeWithFalse2() {
+		verifier.inRange(0, Range.between(1, 2), "");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInRangeWithFalse3() {
+		verifier.inRange(0, Range.between(1, 2), "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testInRangeCmWithFalse() {
+		verifier.inRangeCm(0, Range.between(1, 2), "", null);
 	}
 
 }
