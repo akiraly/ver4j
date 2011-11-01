4b61061bce7b442d6087b909c225c6fc789a1ae1
akiraly <kiralyattila.hu@gmail.com>
2011-11-01 18:03:41 +0100

    Adding order verifying methods.

--- src/main/java/com/github/ver4j/OrderVerifier.java
+++ src/main/java/com/github/ver4j/OrderVerifier.java
@@ -7,15 +7,33 @@ import javax.annotation.Nonnull;
 
 public class OrderVerifier<C extends Comparable<C>> extends
 		AObjectVerifierAwareVerifier {
-	private static final String FAILED_IS_BEFORE_CAUSE = "the comparable object is not ordered before the reference";
+	private static final String FAILED_IS_BEFORE_CAUSE = "the orderable object is not before the reference";
+
+	private static final String FAILED_IS_AFTER_CAUSE = "the orderable object is not after the reference";
+
+	private static final String FAILED_NOT_BEFORE_CAUSE = "the orderable object is before the reference";
+
+	private static final String FAILED_NOT_AFTER_CAUSE = "the orderable object is after the reference";
 
 	private final ExceptionFactory<?> isBeforeExceptionFactory;
+
+	private final ExceptionFactory<?> isAfterExceptionFactory;
+
+	private final ExceptionFactory<?> notBeforeExceptionFactory;
+
+	private final ExceptionFactory<?> notAfterExceptionFactory;
 
 	public OrderVerifier(@Nonnull ObjectVerifier objectVerifier,
 			@Nonnull ExceptionTypeInfo<?> orderExceptionTypeInfo) {
 		super(objectVerifier);
 		isBeforeExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
 				FAILED_IS_BEFORE_CAUSE);
+		isAfterExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
+				FAILED_IS_AFTER_CAUSE);
+		notBeforeExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
+				FAILED_NOT_BEFORE_CAUSE);
+		notAfterExceptionFactory = exceptionFactoryOf(orderExceptionTypeInfo,
+				FAILED_NOT_AFTER_CAUSE);
 	}
 
 	@Nonnull
@@ -53,6 +71,123 @@ public class OrderVerifier<C extends Comparable<C>> extends
 		if (isDisabled() || comparable.compareTo(reference) < 0)
 			return comparable;
 		throw isBeforeExceptionFactory.newExceptionCm(errorMessageTemplate,
+				locale, errorMessageArgs,
+				newComparableContext(comparable, reference), null);
+	}
+
+	@Nonnull
+	public final <T extends C> T isAfter(@Nonnull T comparable,
+			@Nonnull T reference) {
+		object().notNull(comparable);
+		object().notNull(reference);
+		if (isDisabled() || comparable.compareTo(reference) > 0)
+			return comparable;
+		throw isAfterExceptionFactory.newException(
+				newComparableContext(comparable, reference), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T isAfter(@Nonnull T comparable,
+			@Nonnull T reference, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(reference, errorMessage, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) > 0)
+			return comparable;
+		throw isAfterExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, reference),
+				null);
+	}
+
+	@Nonnull
+	public final <T extends C> T isAfterCm(@Nonnull T comparable,
+			@Nonnull T reference, String errorMessageTemplate, Locale locale,
+			Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(reference, errorMessageTemplate, locale,
+				errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) > 0)
+			return comparable;
+		throw isAfterExceptionFactory.newExceptionCm(errorMessageTemplate,
+				locale, errorMessageArgs,
+				newComparableContext(comparable, reference), null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notBefore(@Nonnull T comparable,
+			@Nonnull T reference) {
+		object().notNull(comparable);
+		object().notNull(reference);
+		if (isDisabled() || comparable.compareTo(reference) >= 0)
+			return comparable;
+		throw notBeforeExceptionFactory.newException(
+				newComparableContext(comparable, reference), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notBefore(@Nonnull T comparable,
+			@Nonnull T reference, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(reference, errorMessage, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) >= 0)
+			return comparable;
+		throw notBeforeExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, reference),
+				null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notBeforeCm(@Nonnull T comparable,
+			@Nonnull T reference, String errorMessageTemplate, Locale locale,
+			Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(reference, errorMessageTemplate, locale,
+				errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) >= 0)
+			return comparable;
+		throw notBeforeExceptionFactory.newExceptionCm(errorMessageTemplate,
+				locale, errorMessageArgs,
+				newComparableContext(comparable, reference), null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notAfter(@Nonnull T comparable,
+			@Nonnull T reference) {
+		object().notNull(comparable);
+		object().notNull(reference);
+		if (isDisabled() || comparable.compareTo(reference) <= 0)
+			return comparable;
+		throw notAfterExceptionFactory.newException(
+				newComparableContext(comparable, reference), (Throwable) null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notAfter(@Nonnull T comparable,
+			@Nonnull T reference, Object errorMessage,
+			Object... errorMessageArgs) {
+		object().notNull(comparable, errorMessage, errorMessageArgs);
+		object().notNull(reference, errorMessage, errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) <= 0)
+			return comparable;
+		throw notAfterExceptionFactory.newException(errorMessage,
+				errorMessageArgs, newComparableContext(comparable, reference),
+				null);
+	}
+
+	@Nonnull
+	public final <T extends C> T notAfterCm(@Nonnull T comparable,
+			@Nonnull T reference, String errorMessageTemplate, Locale locale,
+			Object... errorMessageArgs) {
+		object().notNullCm(comparable, errorMessageTemplate, locale,
+				errorMessageArgs);
+		object().notNullCm(reference, errorMessageTemplate, locale,
+				errorMessageArgs);
+		if (isDisabled() || comparable.compareTo(reference) <= 0)
+			return comparable;
+		throw notAfterExceptionFactory.newExceptionCm(errorMessageTemplate,
 				locale, errorMessageArgs,
 				newComparableContext(comparable, reference), null);
 	}
--- src/test/java/com/github/ver4j/OrderVerifierTest.java
+++ src/test/java/com/github/ver4j/OrderVerifierTest.java
@@ -61,6 +61,136 @@ public class OrderVerifierTest {
 
 	@Test(expected = ArgumentOrderVerificationException.class)
 	public void testIsBeforeCmWithFalse() {
-		verifier.isBefore(1, 0, null);
+		verifier.isBeforeCm(1, 0, "", null);
 	}
+
+	// isAfter*
+
+	@Test
+	public void testIsAfterWithTrue() {
+		Integer i = 1;
+		Integer r = 0;
+		Assert.assertSame(i, verifier.isAfter(i, r));
+		Assert.assertSame(i, verifier.isAfter(i, r));
+		Assert.assertSame(i, verifier.isAfter(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.isAfterCm(i, r, "", null));
+	}
+
+	@Test
+	public void testIsAfterWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 1;
+		Integer r = 2;
+		Assert.assertSame(i, verifier.isAfter(i, r));
+		Assert.assertSame(i, verifier.isAfter(i, r));
+		Assert.assertSame(i, verifier.isAfter(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.isAfterCm(i, r, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testIsAfterWithFalse1() {
+		verifier.isAfter(1, 2);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testIsAfterWithFalse2() {
+		verifier.isAfter(1, 2);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testIsAfterWithFalse3() {
+		verifier.isAfter(1, 2, "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testIsAfterCmWithFalse() {
+		verifier.isAfterCm(1, 2, "", null);
+	}
+
+	// notBefore*
+
+	@Test
+	public void testNotBeforeWithTrue() {
+		Integer i = 1;
+		Integer r = 0;
+		Assert.assertSame(i, verifier.notBefore(i, r));
+		Assert.assertSame(i, verifier.notBefore(i, r));
+		Assert.assertSame(i, verifier.notBefore(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.notBeforeCm(i, r, "", null));
+	}
+
+	@Test
+	public void testNotBeforeWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 1;
+		Integer r = 2;
+		Assert.assertSame(i, verifier.notBefore(i, r));
+		Assert.assertSame(i, verifier.notBefore(i, r));
+		Assert.assertSame(i, verifier.notBefore(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.notBeforeCm(i, r, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotBeforeWithFalse1() {
+		verifier.notBefore(1, 2);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotBeforeWithFalse2() {
+		verifier.notBefore(1, 2);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotBeforeWithFalse3() {
+		verifier.notBefore(1, 2, "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotBeforeCmWithFalse() {
+		verifier.notBeforeCm(1, 2, "", null);
+	}
+
+	// notAfter*
+
+	@Test
+	public void testNotAfterWithTrue() {
+		Integer i = 1;
+		Integer r = 2;
+		Assert.assertSame(i, verifier.notAfter(i, r));
+		Assert.assertSame(i, verifier.notAfter(i, r));
+		Assert.assertSame(i, verifier.notAfter(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.notAfterCm(i, r, "", null));
+	}
+
+	@Test
+	public void testNotAfterWithFalseDisabled() {
+		verifier.setDisabled(true);
+		Integer i = 1;
+		Integer r = 0;
+		Assert.assertSame(i, verifier.notAfter(i, r));
+		Assert.assertSame(i, verifier.notAfter(i, r));
+		Assert.assertSame(i, verifier.notAfter(i, r, "%s", "1"));
+		Assert.assertSame(i, verifier.notAfterCm(i, r, "", null));
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotAfterWithFalse1() {
+		verifier.notAfter(1, 0);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotAfterWithFalse2() {
+		verifier.notAfter(1, 0);
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotAfterWithFalse3() {
+		verifier.notAfter(1, 0, "%s", "1");
+	}
+
+	@Test(expected = ArgumentOrderVerificationException.class)
+	public void testNotAfterCmWithFalse() {
+		verifier.notAfterCm(1, 0, "", null);
+	}
+
 }
